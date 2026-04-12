1078194ccf5be3bed4b4e753452615abcc98f9c4
jianliang00 <125073156+jianliang00@users.noreply.github.com>
2026-04-12 18:44:47 +0800

    Add macOS host policy rule manifest

--- Sources/Services/ContainerAPIService/Server/Containers/ContainersService.swift
+++ Sources/Services/ContainerAPIService/Server/Containers/ContainersService.swift
@@ -759,6 +759,7 @@ public actor ContainersService {
         }
 
         let root = self.containerRoot.appendingPathComponent(sandboxID)
+        try MacOSGuestHostNetworkPolicyStore.remove(from: root)
         try MacOSGuestNetworkPolicyStore.remove(from: root)
     }
 

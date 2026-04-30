7a269ff7b630af90cfb4bc97dd00807a55eb67d1
jianliang00 <125073156+jianliang00@users.noreply.github.com>
2026-04-30 23:05:41 +0800

    cni: make macvmnet cleanup idempotent

--- Package.swift
+++ Package.swift
@@ -198,6 +198,7 @@ let package = Package(
         .target(
             name: "ContainerCNIMacvmnet",
             dependencies: [
+                .product(name: "Containerization", package: "containerization"),
                 "ContainerAPIClient",
                 "ContainerNetworkServiceClient",
                 "ContainerResource",
